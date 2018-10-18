09119d1bb494e13bd0594ba51899b5d73384be91
AJ Fite <me@ajfite.com>
2018-10-17 20:42:09 -0700

    working on packaging the GUI

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simf-py-gui",
+    version="0.0.1",
+    author="A.J. Fite, Sara Kipps",
+    author_email="me@ajfite.com, skipps@calpoly.edu",
+    description="A GUI controller for the closed source SIMF camera capture utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.nclf.net/SIMF/simf-python-gui",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
