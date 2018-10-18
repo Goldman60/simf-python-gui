f3ec56fbe2df79bfcd3480113eaa4c7dde1a72a0
AJ Fite <me@ajfite.com>
2018-10-17 20:43:19 -0700

    Change the name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="simf-py-gui",
+    name="simf-python-gui",
     version="0.0.1",
     author="A.J. Fite, Sara Kipps",
     author_email="me@ajfite.com, skipps@calpoly.edu",
