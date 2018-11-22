30ddb0193ed35c1e099f44cf5927f0e34698489d
AJ Fite <me@ajfite.com>
2018-11-21 17:03:27 -0800

    Change versioning strategy

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 import setuptools
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+version_file = open(os.path.join('SimfPythonGUI', 'VERSION'))
+version = version_file.read().strip()
+
 setuptools.setup(
     name="SimfPythonGUI",
-    version="0.0.5",
+    version=version,
     author="A.J. Fite, Sara Kipps",
     author_email="me@ajfite.com, skipps@calpoly.edu",
     description="A GUI controller for the closed source SIMF camera capture utility",
@@ -15,7 +19,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://git.nclf.net/SIMF/simf-python-gui",
     packages=setuptools.find_packages(),
-    package_data={'SimfPythonGUI': ['*.ui']},
+    package_data={'SimfPythonGUI': ['*.ui', 'VERSION']},
     entry_points={
         'gui_scripts': ['simfgui = SimfPythonGUI.guientry:main'],
     },
